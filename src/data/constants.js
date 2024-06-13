9aaa966574fe0f1003518fb1c546ed9bc9b110fe
Stefewn Johnson <81782278+StefewnJohnson@users.noreply.github.com>
2024-06-13 10:53:35 -0400

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -6,7 +6,7 @@ export const Bio = {
       "Website Developer",
     ],
     description:
-      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+      "Hi, I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/StefewnJohnson",
     resume:
       "https://drive.google.com/file/d/1dxoNAgwGSv8gSpxVOBRiWPtL5zVpfMmz/view?usp=sharing",
