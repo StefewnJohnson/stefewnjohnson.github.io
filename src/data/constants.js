d7a27a639b6c8be74b0f7cd26f2f16b81a277c68
StefewnJohnson <stefewn.johnson10@gmai.com>
2024-06-12 21:47:23 -0400

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,258 @@
+export const Bio = {
+    name: "Stefewn Johnson",
+    roles: [
+      "Computer Programmer",
+      "Software Developer",
+      "Website Developer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/StefewnJohnson",
+    resume:
+      "https://drive.google.com/file/d/1dxoNAgwGSv8gSpxVOBRiWPtL5zVpfMmz/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/stefewn-johnson/",
+    github: "https://github.com/StefewnJohnson",
+    //twitter: "https://twitter.com/RishavChanda",
+    //insta: "https://www.instagram.com/rishav_chanda/",
+    //facebook: "https://www.facebook.com/rishav.chanda.165/",
+  };
+  
+  export const skills = [
+    {
+      title: "Languages",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        
+        
+      ],
+    },
+    {
+      title: "Frameworks & Databases",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Next Js",
+          image:
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+      ],
+    },
+    {
+      title: "Tools",
+      skills: [
+        {
+          name: "Android",
+          image:
+            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+        {
+          name: "Git",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvb-AAIFFETEbbKf65WtEmY5q1xg1XFghyDQ&s",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Visual Studio",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+      ],
+    },
+    {
+      title: "Platforms",
+      skills: [ 
+        {
+          name: "Adobe Software",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0gSSPIHfPrsKzUBg7qoY1nmttPvvC1GtaNQ&s",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://www.theglobeandmail.com/resizer/hMhEz1i79KAmXvteNAmF1Es34jc=/arc-anglerfish-tgam-prod-tgam/public/6KE3L65AFNDBJAMVBXZY6ZQGCE.JPG",
+      role: "Graphic Designer",
+      company: "TTC, Toronto, ON",
+      date: "May 2023 - August 2023",
+      desc: "Apply design principles to enhance videos using advanced editing software. Leverage diverse images, fonts, and design techniques to produce compelling visual content. Utilize precise proofreading skills and solid organizational abilities to ensure the highest quality in every project. Thrive in fast-paced environments through adaptable nature and passion for sports. Effectively communicate and collaborate with teams to achieve common goals.",
+      skills: [
+        "Software Design",
+        "User Experience (UX)",
+        "Online Graphics",
+        "Image Design",
+        "Adobe Photoshop",
+        "WordPress",
+        "Microsoft Office",
+      ],
+      //doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+    },
+    {
+      id: 1,
+      img: "https://pbs.twimg.com/profile_images/1306241109924950018/4n0WOra7_400x400.jpg",
+      role: "Head Soccer Coach",
+      company: "Thornhill Soccer Club, Thornhill, ON",
+      date: "May 2023 - PRESENT",
+      desc: "Meticulously craft session plans for training sessions and effectively organize teams to secure victories. Take charge of cleaning and maintaining the sports area, ensuring a safe and welcoming environment for all. Facilitate the development of players' technical skills and soccer knowledge through focused training and mentorship. Cultivate strong leadership, control, and planning skills essential for driving team performance and success.",
+      skills: [
+        "Communication",
+        "Leadership",
+        "Teamwork",
+        "Organization Skills",
+        "Time Management",
+        "Coaching",
+      ],
+      //doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQiXAsA6tjbgJ30S0yxSapjiUCgheDWmVAEw&s",
+      role: "Sports Attendant",
+      company: "City of Vaughan",
+      date: "Apr 2022 - May 2024",
+      desc: "Teach children various sports, prioritizing their enjoyment and safety in a positive and supportive environment. Take responsibility for cleanliness and maintenance of sports area, ensuring safe and hygienic space for activities. Hone strong verbal communication and interpersonal skills, effectively engaging with children and colleagues. Develop solid organizational skills to manage tasks efficiently. Cultivate collaborative skills by working closely with teams, contributing to a cohesive and effective team dynamic.",
+      skills: [
+        "Communication",
+        "Interpersonal Skills",
+        "Teaching",
+        "Safety Awareness",
+        "Risk Management",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8rHrGdb-8ROOm1Y0iaZQ7fczblKh4mqab_Q&s",
+      school: "York University, North York, ON",
+      date: "Sep 2022 - May 2026",
+      grade: "7.0/9.0 GPA",
+      desc: "Activities and societies: I am currently enrolled in the prestigious Bergeron Entrepreneurs in Science and Technology certification program. This unique program combines courses from the esteemed Lassonde School of Engineering, Schulich School of Business, and Osgoode Hall Law School. It offers a comprehensive blend of technical expertise, business acumen, and legal understanding, preparing me to excel in the dynamic intersection of science, technology, and entrepreneurship.",
+      degree: "Hons.Bachelor of Arts, Computer Science",
+    },
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4mbIzvW76PTadcpcO-RLCC4e-VwcShkfQ7Q&s",
+      school: "Cardinal Carter High School, Aurora, ON",
+      date: "Sep 2018 - Jun 2022",
+      grade: "95.0%",
+      desc: "Activities and societies: Deca Club | Soccer Club | Stock Club",
+      degree: "High School Diploma",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Emotion Detector",
+      date: "Apr 2024 - Jun 2024",
+      description:
+        "A trained AI model able to detect human emotion",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Google_Colaboratory_SVG_Logo.svg/1280px-Google_Colaboratory_SVG_Logo.svg.png",
+      tags: ["Python", "Google Cloud", "GBT2", "Microsoft Power BI", "Google Vertex AI"],
+      category: "machine learning",
+      webapp: "https://github.com/StefewnJohnson/Emotion-Detector",
+    },
+    {
+      id: 1,
+      title: "Edify",
+      date: "Oct 2023 - Dec 2023",
+      description:
+        "Development of a comprehensive educational initiative",
+      image:
+        "https://edifylearning.github.io/EdifyWebsite/assets/img/Logo%20Edify%20(11)%20(1).png",
+      tags: [
+        "HTML",
+        "CSS",
+      ],
+      category: "website",
+      webapp: "https://edifylearning.github.io/EdifyWebsite/",
+    },
+    {
+      id: 2,
+      title: "Awesome Platformer",
+      date: "Sep 2021 - June 2021",
+      description:
+        "A game created in unity with the goal of killing all enemies in your path and reaching the end point",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1CexuobW8gDuSRR4lE3cQ7CMwNmW9mBhSgg&s",
+      tags: ["C#, Unity"],
+      category: "game",
+      webapp: "https://github.com/StefewnJohnson/Awesome-Platformer",
+      //webapp: "https://trackify-management.netlify.app/",
+    },
+    {
+      id: 3,
+      title: "Hangman Program",
+      date: "Mar 2022 - Apr 2022",
+      description:
+        "A simple hangman game created in Java to replicate playing the game in real life",
+      image:
+        "https://media.istockphoto.com/id/1196954772/vector/simple-illustration-of-hangman-game.jpg?s=612x612&w=0&k=20&c=Z_Sxdqu4i100u0qeSsVdX_M--VoXgPISK83gBJCf3LM=",
+      tags: ["Java", "Eclipse"],
+      category: "program",
+      webapp: "https://github.com/StefewnJohnson/Hangman-Game",
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
